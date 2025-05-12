0311b34f5c87a1cce7b5a4bc58f8050e3b7d55c2
Aleksei <zidvi33@gmail.com>
2025-05-12 23:55:31 +0300

    fix: debug timeout

--- tests/selectMenu.test.ts
+++ tests/selectMenu.test.ts
@@ -4,8 +4,12 @@ import { SelectMenuPage } from '../pages/SelectMenuPage';
 test('Select dropdown options', async ({ page }) => {
   const selectMenuPage = new SelectMenuPage(page);
 
-  await page.goto('https://demoqa.com/select-menu');
-  await page.waitForLoadState('domcontentloaded');
+  await page.goto('https://demoqa.com/select-menu', {
+    timeout: 60000,
+    waitUntil: 'domcontentloaded'
+  });
+
+  await page.waitForSelector('#selectMenuContainer', { timeout: 15000 });
 
   await selectMenuPage.selectDropdownValues();
 });
