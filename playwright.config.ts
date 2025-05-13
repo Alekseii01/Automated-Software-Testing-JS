f7213ca9fa6c834a04d22e673ad937d67a844c19
Aleksei <zidvi33@gmail.com>
2025-05-13 14:22:41 +0300

    fix: change workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
         { name: 'Firefox@1366x768', use: { browserName: 'firefox', viewport: { width: 1366, height: 768 } } },
     ],
     retries: 2,
-    workers: 2,
+    workers: 10,
     timeout: 50000,
     expect: {
     timeout: 10000
